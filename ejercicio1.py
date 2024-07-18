8f647db1f6d2a990af5803b5f30d3be704be324e
Solll Monsalve <solanyimonsalve25@gmail.com>
2024-07-17 22:04:58 -0500

    first commit

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+print("probando python con visual studio code")
+print("La felicidad se puede encontrar hasta en los más oscuros momentos")
+print ("si somos capaces de usar bienla luz.")
+print("-Albus Dumbledore")
+
+
+
+print('''La felicidad se puede encontrar hasta en los más oscuros momentos 
+      si somos capaces de usar bienla luz. 
+      -Albus Dumbledore''')
